19f7eca05ff7f6089769e40f7eb6c665597efbe9
Allen Houng <ayhoung@gmail.com>
2022-04-25 15:42:41 +0800

    disabled lightbar for now

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,14 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<div className="relative select-none"><h1 className="text-white mt-5 ml-5 text-3xl">EMDR Virtual Lightbar</h1></div>
-			<div className={styles.pot} style={{'-webkit-animation-duration': `${seconds}s`}}>
+			{/* <div className="relative select-none"><h1 className="text-white mt-5 ml-5 text-3xl">EMDR Virtual Lightbar</h1></div> */}
+			{/* <div className={styles.pot} style={{'-webkit-animation-duration': `${seconds}s`}}> */}
 				{/* <Image src="https://i.stack.imgur.com/qgNyF.png?s=328&g=1" width="100px" height="100px" alt="indicator" /> */}
-				<FireIcon className="h-20 w-20" color="white"/>
-			</div>
-			<div className="absolute bottom-10 flex justify-center w-full select-none">
-				<p className="text-white select-none">{displaySpeed.toFixed(1)}</p>
+				{/* <FireIcon className="h-20 w-20" color="white"/> */}
+			{/* </div> */}
+			<div className="absolute bottom-10 flex justify-center w-full select-none text-white">
+				{/* <p className="text-white select-none">{displaySpeed.toFixed(1)}</p> */}
+				Preview mode....
 			</div>
 			<div className="absolute right-10 bottom-10 select-none">
 				<ArrowCircleUpIcon 
